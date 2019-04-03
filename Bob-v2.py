01717572c2d207e8e4eba5a29409b8db5ed45c4c
Axel <axeldvi.pro@gmail.com>
2019-04-03 23:24:16 +0200

    working in prod

--- Bob-v2.py
+++ Bob-v2.py
@@ -12,6 +12,23 @@ GPIO.add_event_detect(14, GPIO.FALLING)
 
 #FUNCTIONS ----------------------------------------------------------------------------------------------------------------------------------
 
+def textHollow(font, message, fontcolor):
+    notcolor = [c^0xFF for c in fontcolor]
+    base = font.render(message, 0, fontcolor, notcolor)
+    size = base.get_width() + 2, base.get_height() + 2
+    img = pygame.Surface(size, 16)
+    img.fill(notcolor)
+    base.set_colorkey(0)
+    img.blit(base, (0, 0))
+    img.blit(base, (2, 0))
+    img.blit(base, (0, 2))
+    img.blit(base, (2, 2))
+    base.set_colorkey(0)
+    base.set_palette_at(1, notcolor)
+    img.blit(base, (1, 1))
+    img.set_colorkey(notcolor)
+    return img
+
 def textOutline(font, message, fontcolor, outlinecolor):
     base = font.render(message, 0, fontcolor)
     outline = textHollow(font, message, outlinecolor)
